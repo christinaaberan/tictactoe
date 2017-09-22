2a287180169cf7ade275e333ed492e4d34daa858
christinaaberan <christinaaberan@gmail.com>
2017-09-22 10:48:17 -0400

    Added some styling

--- script.js
+++ script.js
@@ -22,40 +22,57 @@ window.onload = function() {
 			if( counter % 2 === 0){
 				this.innerHTML = "<h2>X</h2>";
 				this.classList.add("touched");
+				this.classList.add("x");
 				checkItOut();
+				
 			}
 			else{
 				this.innerHTML = "<h2>O</h2>";
-				this.classList.add("otouched");
+				this.classList.add("touched");
+				this.classList.add("o");
 				checkItOut();
+			
 			}
 		}
 
 		counter++;
 	}
+
+	function replaceTxt(){
+		for (var e=0; e<space.length; e++){
+			if(space[e].classList.contains("touched")){
+				console.log("hi");
+			}
+			else{
+				space[e].innerHTML = "<span><i class='fa fa-smile-o' aria-hidden='true'></i></span>";
+			}
+		}
+	}
 	
 	function checkItOut(){
-		if((space[0].classList.contains("touched"))&&(space[3].classList.contains("touched"))&&(space[6].classList.contains("touched"))||
-			(space[0].classList.contains("touched"))&&(space[1].classList.contains("touched"))&&(space[2].classList.contains("touched"))||
-			(space[0].classList.contains("touched"))&&(space[4].classList.contains("touched"))&&(space[8].classList.contains("touched"))||
-			(space[3].classList.contains("touched"))&&(space[4].classList.contains("touched"))&&(space[5].classList.contains("touched"))||
-			(space[6].classList.contains("touched"))&&(space[7].classList.contains("touched"))&&(space[8].classList.contains("touched"))||
-			(space[1].classList.contains("touched"))&&(space[4].classList.contains("touched"))&&(space[7].classList.contains("touched"))||
-			(space[2].classList.contains("touched"))&&(space[5].classList.contains("touched"))&&(space[8].classList.contains("touched"))||
-			(space[2].classList.contains("touched"))&&(space[4].classList.contains("touched"))&&(space[6].classList.contains("touched"))){
+		if((space[0].classList.contains("x"))&&(space[3].classList.contains("x"))&&(space[6].classList.contains("x"))||
+			(space[0].classList.contains("x"))&&(space[1].classList.contains("x"))&&(space[2].classList.contains("x"))||
+			(space[0].classList.contains("x"))&&(space[4].classList.contains("x"))&&(space[8].classList.contains("x"))||
+			(space[3].classList.contains("x"))&&(space[4].classList.contains("x"))&&(space[5].classList.contains("x"))||
+			(space[6].classList.contains("x"))&&(space[7].classList.contains("x"))&&(space[8].classList.contains("x"))||
+			(space[1].classList.contains("x"))&&(space[4].classList.contains("x"))&&(space[7].classList.contains("x"))||
+			(space[2].classList.contains("x"))&&(space[5].classList.contains("x"))&&(space[8].classList.contains("x"))||
+			(space[2].classList.contains("x"))&&(space[4].classList.contains("x"))&&(space[6].classList.contains("x"))){
 			whoWon.innerHTML = "X Wins!!";
 			event.preventDefault();
+			replaceTxt();
 		}
-		else if((space[0].classList.contains("otouched"))&&(space[3].classList.contains("otouched"))&&(space[6].classList.contains("otouched"))||
-			(space[0].classList.contains("otouched"))&&(space[1].classList.contains("otouched"))&&(space[2].classList.contains("otouched"))||
-			(space[0].classList.contains("otouched"))&&(space[4].classList.contains("otouched"))&&(space[8].classList.contains("otouched"))||
-			(space[3].classList.contains("otouched"))&&(space[4].classList.contains("otouched"))&&(space[5].classList.contains("otouched"))||
-			(space[6].classList.contains("otouched"))&&(space[7].classList.contains("otouched"))&&(space[8].classList.contains("otouched"))||
-			(space[1].classList.contains("otouched"))&&(space[4].classList.contains("otouched"))&&(space[7].classList.contains("otouched"))||
-			(space[2].classList.contains("otouched"))&&(space[5].classList.contains("otouched"))&&(space[8].classList.contains("otouched"))||
-			(space[2].classList.contains("otouched"))&&(space[4].classList.contains("otouched"))&&(space[6].classList.contains("otouched"))){
+		else if((space[0].classList.contains("o"))&&(space[3].classList.contains("o"))&&(space[6].classList.contains("o"))||
+			(space[0].classList.contains("o"))&&(space[1].classList.contains("o"))&&(space[2].classList.contains("o"))||
+			(space[0].classList.contains("o"))&&(space[4].classList.contains("o"))&&(space[8].classList.contains("o"))||
+			(space[3].classList.contains("o"))&&(space[4].classList.contains("o"))&&(space[5].classList.contains("o"))||
+			(space[6].classList.contains("o"))&&(space[7].classList.contains("o"))&&(space[8].classList.contains("o"))||
+			(space[1].classList.contains("o"))&&(space[4].classList.contains("o"))&&(space[7].classList.contains("o"))||
+			(space[2].classList.contains("o"))&&(space[5].classList.contains("o"))&&(space[8].classList.contains("o"))||
+			(space[2].classList.contains("o"))&&(space[4].classList.contains("o"))&&(space[6].classList.contains("o"))){
 			whoWon.innerHTML = "O Wins!!";
 			event.preventDefault();
+			replaceTxt();
 		}
 	}
 
